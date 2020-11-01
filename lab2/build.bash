0c26b6b96f630eed63fd132b7569e43c2e8398d3
maxim <maxim.univer27@outlook.com>
2020-11-01 15:55:59 +0300

    Fix build.bash

--- lab2/build.bash
+++ lab2/build.bash
@@ -1,4 +1,4 @@
-mkdir lib 2>/dev/null
+mkdir bin 2>/dev/null
 echo -e "class-path: lib/Pokemon.jar\nmain-class: BattleSimulation" > .mf
 javac -encoding UTF-8 -cp lib/* -d bin src/*.java src/moves/*.java src/pokemons/*.java
 jar -cmf .mf PokemonBattle.jar -C bin .
