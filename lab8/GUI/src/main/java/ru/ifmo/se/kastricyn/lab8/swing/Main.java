cc3ce3108736f497512ec379a40d2ad7621b058f
Maks <mikastricyn@outlook.com>
2021-08-23 17:21:47 +0300

    Add new AuthForm (not worked yet)

--- lab8/GUI/src/main/java/ru/ifmo/se/kastricyn/lab8/swing/Main.java
+++ lab8/GUI/src/main/java/ru/ifmo/se/kastricyn/lab8/swing/Main.java
@@ -5,8 +5,10 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new AuthForm();
+            JFrame frame = new JFrame();
+            frame.setContentPane(new AuthForm().getLogInPanel());
             frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         });
     }
 }
